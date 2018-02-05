6fad5582b9356ec3c781eb5b92811ef50c7bdb0b
Tamás Németh <35818300+sugarman402@users.noreply.github.com>
2018-02-05 20:30:03 +0100

    just comments
    comments

--- smb.bat
+++ smb.bat
@@ -1,7 +1,11 @@
+::Enable SMBv1 and SMBv2/v3 in Windows 10
+::Command-line
 sc.exe config lanmanworkstation depend= bowser/mrxsmb10/mrxsmb20/nsi
 sc.exe config mrxsmb10 start= auto
 sc.exe config mrxsmb20 start= auto
+::registry
 reg add HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters /v SMB1 /t REG_DWORD /d 1 /f
 reg add HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters /v SMB2 /t REG_DWORD /d 1 /f
+::PowerShell
 powershell set-smbserverconfiguration -enablesmb1protocol $true -force
 powershell set-smbserverconfiguration -enablesmb2protocol $true -force
