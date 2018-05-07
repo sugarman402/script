b66fdab4ec37225db108feeaba4895a027b0c12a
Tamás Németh <35818300+sugarman402@users.noreply.github.com>
2018-05-07 08:27:35 +0200

    Update smb.bat

--- smb.bat
+++ smb.bat
@@ -7,5 +7,6 @@ sc.exe config mrxsmb20 start= auto
 reg add HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters /v SMB1 /t REG_DWORD /d 1 /f
 reg add HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters /v SMB2 /t REG_DWORD /d 1 /f
 ::PowerShell
+powershell Enable-WindowsOptionalFeature -Online -FeatureName smb1protocol
 powershell set-smbserverconfiguration -enablesmb1protocol $true -force
 powershell set-smbserverconfiguration -enablesmb2protocol $true -force
